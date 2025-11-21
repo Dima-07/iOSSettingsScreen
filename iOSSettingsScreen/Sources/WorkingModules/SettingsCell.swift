1cfdaa22c7ad69c74b27114ab043bb7a2d57c160
Dmitriy <dima_kondratenko04@mail.ru>
2025-11-21 14:42:42 +0300

    Add settings cell.

--- iOSSettingsScreen/Sources/WorkingModules/SettingsCell.swift
+++ iOSSettingsScreen/Sources/WorkingModules/SettingsCell.swift
@@ -10,8 +10,46 @@ import UIKit
 class SettingsCell: UITableViewCell {
 
     // MARK: - Outlets
-
-
+    
+    private let iconBackgroundView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = 8
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .white
+        imageView.preferredSymbolConfiguration = .init(pointSize: 14, weight: .regular)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17)
+        label.numberOfLines = 0
+        label.setContentCompressionResistancePriority(.required, for: .horizontal)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let detailLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 15)
+        label.textColor = .secondaryLabel
+        label.textAlignment = .right
+        label.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    
     // MARK: - Initializers
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -27,14 +65,46 @@ class SettingsCell: UITableViewCell {
     // MARK: - Setup
 
     private func setupHierarchy() {
+        contentView.addSubview(iconBackgroundView)
+        iconBackgroundView.addSubview(iconImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(detailLabel)
     }
 
     private func setupLayout() {
         NSLayoutConstraint.activate([
-
+            iconBackgroundView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            iconBackgroundView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            iconBackgroundView.widthAnchor.constraint(equalToConstant: 30),
+            iconBackgroundView.heightAnchor.constraint(equalToConstant: 30),
+            
+            iconImageView.centerYAnchor.constraint(equalTo: iconBackgroundView.centerYAnchor),
+            iconImageView.centerXAnchor.constraint(equalTo: iconBackgroundView.centerXAnchor),
+            iconImageView.widthAnchor.constraint(equalToConstant: 20),
+            iconImageView.heightAnchor.constraint(equalToConstant: 20),
+            
+            titleLabel.leadingAnchor.constraint(equalTo: iconBackgroundView.trailingAnchor, constant: 12),
+            titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            
+            detailLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -3),
+            detailLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
         ])
     }
 
     // MARK: - Reuse
-
+    func configure( with item: SettingsModel) {
+        iconBackgroundView.backgroundColor = item.backgroundColor
+        iconImageView.image = item.icon
+        titleLabel.text = item.title
+        detailLabel.text = item.detailText
+        detailLabel.isHidden = item.detailText == nil
+        
+        if item.firstCell {
+            iconBackgroundView.layer.cornerRadius = 30
+            iconImageView.contentMode = .scaleAspectFill
+        } else {
+            iconBackgroundView.layer.cornerRadius = 8
+            iconImageView.contentMode = .scaleAspectFit
+        }
+    }
 }
